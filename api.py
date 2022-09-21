01a68b9fc6e6d68a168349ef866a99435d1d6553
Moses Jasi <mjjustme26@gmail.com>
2022-09-21 10:35:08 +0200

    Added SQLAlchemy implementing SQLite

--- api.py
+++ api.py
@@ -1,37 +1,88 @@
+
+from email import message
 from typing_extensions import Required
 from urllib import request
 from flask import Flask
-from flask_restful import Resource, Api, reqparse, abort
+from flask_restful import Resource, Api, reqparse, abort,fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+db = SQLAlchemy(app)
 
-todos = {
-    1: {"task" : "Task 1" , "summary": "write code using python"},
-    2: {"task" : "Task 2" , "summary": "write code using python"},
-    3: {"task" : "Task 3" , "summary": "write code using python"},
-    4: {"task" : "Task 4" , "summary": "write code using python"},
-    5: {"task" : "Task 5" , "summary": "write code using python"},
-}
+
+class ToDoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200))
+    summary = db.Column(db.String(500))
+
+# db.create_all()
+
+
 
 task_post_args = reqparse.RequestParser()
 task_post_args.add_argument("task", type=str, help="Task is required", required=True)
 task_post_args.add_argument("summary", type=str, help="Summary is required", required=True)
 
-class ToDo(Resource):
-    def get(self, todo_id):
-        return todos[todo_id]
+task_update_args = reqparse.RequestParser()
+task_update_args.add_argument("task", type=str)
+task_update_args.add_argument("summary", type=str)
 
-    def post(self, todo_id):
-        args = task_post_args.parse_args()
-        if todo_id in todos:
-            abort(409, "Task already taken")
-        todos[todo_id] = {"task" : args["task"], "summary" : args["summary"] }
-        return todos
+resource_fields ={
+    'id' : fields.Integer,
+    'task' : fields.String,
+    'summary' : fields.String
+}
 
 class ToDoList(Resource):
     def get(self):
-        return todos
+     tasks = ToDoModel.query.all()
+     todos = {}
+     for task in tasks:
+        todos[task.id] = {"task" : task.task, "summary" : task.summary}
+     return todos
+
+
+class ToDo(Resource):
+    @marshal_with(resource_fields)
+    def get(self, todo_id):
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(409, "Cpuld not find task with that id")
+        return task
+
+    @marshal_with(resource_fields)
+    def post(self, todo_id):
+        args = task_post_args.parse_args()
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if task:
+            abort(409, "Task ID already taken")
+        todo = ToDoModel(id= todo_id, task = args['task'], summary = args['summary'])
+        db.session.add(todo)
+        db.session.commit()
+        return todo, 201
+    
+
+    @marshal_with(resource_fields)
+    def put(self, todo_id):  
+        args = task_update_args.parse_args()
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        if not task:
+            abort(404, message="Task does not exist, can not update")
+        if args['task']:
+            task.task = args['task']
+        if args['summary']:
+            task.summary = args['summary']
+        db.session.commit()
+        return task
+
+    def delete(self, todo_id):
+        task = ToDoModel.query.filter_by(id=todo_id).first()
+        db.session.delete(task)
+        db.session.commit()
+        return 'Todo Deleted', 204
+
 
 api.add_resource(ToDo, '/todos/<int:todo_id>')
 api.add_resource(ToDoList, '/todos')
